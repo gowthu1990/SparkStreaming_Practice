824e2740a44c62c58c50d330ce589eca993957bd
Sai Gowtham Badvity <gowtham.badvity@gmail.com>
2022-09-24 19:56:01 +0100

    Json Streams

---
+++ src/main/scala/com/clearurdoubt/common/CommonSchemas.scala
@@ -0,0 +1,11 @@
+package com.clearurdoubt.common
+
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+object CommonSchemas {
+  val studentSchema: StructType = StructType(Array(
+    StructField("name", StringType, nullable = true),
+    StructField("id", IntegerType, nullable = true),
+    StructField("class", StringType, nullable = true)
+  ))
+}
--- src/main/scala/com/clearurdoubt/streaming/StreamingDFs.scala
+++ src/main/scala/com/clearurdoubt/streaming/StreamingDFs.scala
@@ -1,8 +1,9 @@
 package com.clearurdoubt.streaming
 
+import com.clearurdoubt.common.CommonSchemas.studentSchema
 import com.clearurdoubt.common.SparkUtils
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.functions.{col, count, from_json}
 import org.apache.spark.sql.streaming.Trigger
 
 import scala.concurrent.duration.DurationInt
@@ -22,13 +23,30 @@ object StreamingDFs {
 
     transformedDF.writeStream
       .format("console")
-      .outputMode("complete") // append and update are not not supported with aggregations
+      .outputMode("complete") // append and update are not supported with aggregations
       .trigger(Trigger.ProcessingTime(2.seconds))
       .start()
       .awaitTermination()
   }
 
+  def readJsonSocketStreams(): Unit = {
+    val df: DataFrame = SparkUtils.getSparkSession(isLocal = true, appName = "Json Socket Streams")
+      .readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 12345)
+      .load()
+      .select(from_json(col("value"), studentSchema).as("students"))
+      .selectExpr("students.*")
+
+    df.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
+  }
+
   def main(args: Array[String]): Unit = {
-    readSocketStreams()
+    readJsonSocketStreams()
   }
 }
